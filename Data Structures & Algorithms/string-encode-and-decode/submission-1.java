42eb7eecdf1e8045b237cf711f5cf2422a90a3de
Luiz Henrique Souza Andrade <luizsouzandrade.dev@gmail.com>
2026-07-06 15:52:36 -0300

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,33 @@
+
+public class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+ 
+            sb.append(s.length()).append("あ").append(s);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+          
+            while (str.charAt(j) != 'あ') {
+                j++;
+            }
+      
+            int length = Integer.parseInt(str.substring(i, j));
+
+            i = j + 1;
+          
+            res.add(str.substring(i, i + length));
+
+            i += length;
+        }
+        return res;
+    }
+}
